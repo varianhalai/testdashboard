929e7ac8ded8bf88adb467d8a01815aba289e563
Byron-McKeeby <byron.mckeeby@protonmail.com>
2020-09-11 12:37:26 -0700

    track script

---
+++ scripts/track.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+
+const harvest = require('../src/lib/manager.js');
+const {prettyPosition, prettyUnderlying} = require('../src/lib/utils.js');
+
+const INTERVAL = 5 * 60 * 1000; // 5 minutes
+
+const {ethers} = harvest;
+
+const me = process.argv[2];
+const infuraId = process.argv[3];
+
+const FILE = `./scripts/bal_logs_${me}.json`;
+
+if (!fs.existsSync(FILE)) {
+  fs.writeFileSync(FILE, JSON.stringify({}));
+}
+
+/**
+ * @param {string} address address
+ * @param {string} infuraId infuraId
+ */
+async function logBalances(address, infuraId) {
+  const provider = new ethers.providers.InfuraProvider(
+      'homestead',
+      infuraId,
+  );
+  const man = harvest.PoolManager.allPastPools(provider);
+  const summaries = await man.summary(address);
+  const underlyings = await man.underlying(address);
+
+  let totalRewards = ethers.BigNumber.from(0);
+
+  const positions = summaries
+      .map(prettyPosition)
+      .filter((p) => p.earnedRewards !== '0.0' || p.stakedBalance !== '0.0');
+
+  summaries.forEach((pos) => {
+    totalRewards = totalRewards.add(pos.summary.earnedRewards);
+  });
+
+  const output = {
+    positions,
+    underlyings: underlyings.map(prettyUnderlying).filter((u) => !!u)};
+
+
+  console.log('------LOGGING OUTPUT-----');
+  const timestamp = Date.now();
+  console.log(`The time is ${timestamp}`);
+  console.table('REWARDS');
+  console.table(output.positions);
+  console.log(
+      `Total claimable rewards: ${ethers.utils.formatEther(totalRewards)}`,
+  );
+  console.log('');
+
+  output.underlyings.forEach((u) => {
+    console.log(u.asset);
+    console.table(u.underlyingBalances);
+    console.log('');
+  });
+
+  console.log('------DUMPING TO FILE-----');
+
+  const history = JSON.parse(fs.readFileSync(FILE, 'utf8'));
+  history[String(timestamp)] = output;
+  fs.writeFileSync(FILE, JSON.stringify(history));
+}
+
+logBalances(me, infuraId);
+setInterval(() => logBalances(me, infuraId), INTERVAL);
