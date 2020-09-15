a53fb2b712abd6fed91aff31dd712adb7fcaf441
Byron-McKeeby <byron.mckeeby@protonmail.com>
2020-09-15 08:48:00 -0700

    improve deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
+CURRENT_BRANCT=$(git rev-parse --abbrev-ref HEAD)
 CURRENT_COMMIT=$(git rev-parse HEAD)
 git checkout -b tmp-gh-pages && \
 git add -f ./build && \
-git commit -m "$CURRENT_COMMIT" && \
+git commit -m "$CURRENT_BRANCH @ $CURRENT_COMMIT" && \
 git subtree split --prefix build -b gh-pages && \
 git push -f origin gh-pages:gh-pages && \
-git checkout master
+git checkout $CURRENT_BRANCH
 
 # try this regardless
 git branch -D tmp-gh-pages
