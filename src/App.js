9bf92a4c776998086dd6fccb4eee7fc983057d13
varianhalai <bastardsinspace@gmail.com>
2020-10-31 19:02:06 -0500

    reduced margin-top of rows by 20px

--- src/App.js
+++ src/App.js
@@ -464,7 +464,7 @@ function App() {
                       </Col>
                     </Row>
 
-                    <Row style={{marginTop:"35px"}}>
+                    <Row style={{marginTop:"15px"}}>
                       <Col lg="6">
                         <Harvest state={state} />
                       </Col>
@@ -477,7 +477,7 @@ function App() {
                       
                       
                       </Row>
-                      <Row style={{marginTop:"35px"}}>
+                      <Row style={{marginTop:"15px"}}>
                       <Col lg ="6">
                         <AddTokens state={state} />
                       </Col>
