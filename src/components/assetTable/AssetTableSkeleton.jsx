1d8a7b9dacab948f6d356967a36975c12c1bdce1
varianhalai <bastardsinspace@gmail.com>
2020-12-08 21:06:49 -0600

    Improved the design of the assetTable skeleton component.

--- src/components/assetTable/AssetTableSkeleton.jsx
+++ src/components/assetTable/AssetTableSkeleton.jsx
@@ -10,21 +10,19 @@ flex-direction: column;
 align-items: center;
 width: 100%;
 height: 20rem;
-background: none;
+background-color: ${(props) => props.theme.style.panelBackground};
 border: ${(props) => props.theme.style.mainBorder};
 box-shadow: ${(props) => props.theme.style.panelBoxShadow};
 border-radius: .5rem;
 overflow: hidden;
-
-
+position: relative;
+z-index: 300;
     .skeleton-header {
         margin: 0;
         height: 6rem;
         width: 100%;
         border-bottom: 2px solid black;
-
     }
-
     &::before {
         content: '';
         position: relative;
@@ -35,7 +33,6 @@ overflow: hidden;
         background: linear-gradient(to right, transparent 10%, ${(props) => props.theme.style.blueBackground} 50%, transparent 100%);
         animation: load 1.8s cubic-bezier(0.4, 0.0, 0.2, 1) infinite;
     }
-
     @keyframes load {
         from {
             left: -100%;
@@ -44,15 +41,39 @@ overflow: hidden;
             left: 100%;
         }
     }
-
     &.stake-skeleton
+`;
 
 
+const PanelTabContainerLeft = styled.div`
+  display: flex;
+  justify-content: flex-start;
+`;
+
+const PanelTab = styled.div`        
+ width: 240px;
+ height: 4.5rem;
+  margin-right: 0.75rem;
+  border-radius: 1.2rem;
+  border-top: ${(props) => props.theme.style.mainBorder};
+  border-left: ${(props) => props.theme.style.mainBorder};
+  border-right: ${(props) => props.theme.style.mainBorder};
+  padding: 0.75rem 2rem 2rem 2rem;
+  background-color: transparent;
+  box-shadow: ${(props) => props.theme.style.panelTabBoxShadow};
+  position: relative;
+  top: 1.2rem;
+  color: ${(props) => props.theme.style.buttonFontColor};
 `;
 
 const AssetTableSkeleton = ({state}) => {
     return (
         <ThemeProvider theme={state.theme === "dark" ? darkTheme : lightTheme}>
+            <PanelTabContainerLeft> 
+          <PanelTab>
+           
+          </PanelTab> 
+          </PanelTabContainerLeft>
             <SkeletonPanel>
                 
                 
