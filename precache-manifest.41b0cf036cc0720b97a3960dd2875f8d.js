96ebce732b1a421bf898b9c0423b24adb5238eb9
varianhalai <bastardsinspace@gmail.com>
2020-12-12 18:34:35 -0600

    feature/redesign @ c1e03c2a5aa213058799e96f27673ab7122e1d8e

---
+++ precache-manifest.41b0cf036cc0720b97a3960dd2875f8d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d70e079fb72f905f08d10701574fce3",
+    "url": "/testdashboard/index.html"
+  },
+  {
+    "revision": "a7e9a4800d4458a3748d",
+    "url": "/testdashboard/static/js/2.fa86d7a6.chunk.js"
+  },
+  {
+    "revision": "0742f2e7ad8625dcd2fe96b3af4ec91b",
+    "url": "/testdashboard/static/js/2.fa86d7a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8ab020291c12363b27b",
+    "url": "/testdashboard/static/js/main.abe93352.chunk.js"
+  },
+  {
+    "revision": "f14ca80e2bdb22798549",
+    "url": "/testdashboard/static/js/runtime-main.28fee9dc.js"
+  },
+  {
+    "revision": "b58cf5eb323061da6d9d0af7977c5214",
+    "url": "/testdashboard/static/media/HarvestRadio.b58cf5eb.mp3"
+  },
+  {
+    "revision": "575896520020fbd0f4ea1674ceda4b3f",
+    "url": "/testdashboard/static/media/eth_dai.57589652.svg"
+  },
+  {
+    "revision": "dfbb5e3792d89e39958c3a61081268df",
+    "url": "/testdashboard/static/media/eth_usdc.dfbb5e37.svg"
+  },
+  {
+    "revision": "1bcf41fd64af68e2d586c3a005d32f29",
+    "url": "/testdashboard/static/media/eth_usdt.1bcf41fd.svg"
+  },
+  {
+    "revision": "de8940ed6afd4037ee2c397de352c9bb",
+    "url": "/testdashboard/static/media/eth_wbtc.de8940ed.svg"
+  },
+  {
+    "revision": "50ba3f462c6db64504cb86cc419e677e",
+    "url": "/testdashboard/static/media/gif_tractor.50ba3f46.gif"
+  },
+  {
+    "revision": "03f6920767840988f31f595cc59607be",
+    "url": "/testdashboard/static/media/sushi_wbtc.03f69207.svg"
+  }
+]);
